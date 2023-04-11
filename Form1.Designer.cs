185f20a2a84dab8d027022b233e0c1a83f21bc77
user <tarnatstv748@gmail.com>
2023-04-11 14:35:39 +0300

    Add 2048 game

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,50 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(57, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 70);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "8";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(277, 161);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "label2";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(441, 324);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(755, 703);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "2048";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+       
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,25 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2048
 {
     public partial class Form1 : Form
     {
+        private const int mapSize = 4;
+        private Label[,] labelMap;
+        private static Random random=new Random();
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            LoadMap();
+            GenerateNumber();
+        }
 
+        private void GenerateNumber()
+        {
+            var randomNumberLabel = random.Next(mapSize * mapSize);
+            var indexRow = randomNumberLabel / mapSize;
+            var indexColumn=randomNumberLabel % mapSize;
+            if (labelMap[indexRow, indexColumn].Text==string.Empty)
+            {
+                labelMap[indexRow, indexColumn].Text = "2";
+            }
+            else
+            {
+                GenerateNumber();
+            }
+
+        }
+
+        private void LoadMap()
+        {
+            labelMap = new Label[mapSize, mapSize];
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    var newLable = CreateLabel(i, j);
+                    Controls.Add(newLable);
+                    labelMap[i, j] = newLable;
+                }
+            }
+        }
+
+        private Label CreateLabel(int indexRow, int indexColumn)
+        {
+            var label = new Label();
+
+            label.BackColor = SystemColors.ControlDark;
+            label.Font = new Font("Microsoft Sans Serif", 18F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(204)));
+            label.Size = new Size(70, 70);
+            label.TabIndex = 0;
+            int x = 10 + indexColumn * 76;
+            int y = 70 + indexRow * 76;
+            label.TextAlign = ContentAlignment.MiddleCenter;
+            label.Location = new Point(x, y);
+            return label;
+
+
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+
+            }
         }
     }
 }
