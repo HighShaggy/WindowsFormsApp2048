654f74d023e2dff4aec98ddbcc45fd974192a65d
user <tarnatstv748@gmail.com>
2023-04-18 08:59:55 +0300

    Add switch game mode

---
+++ ChangeModeGame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2048
+{
+    public partial class ChangeModeGame : Form
+    {
+        public List<RadioButton> radioButtons; 
+        public ChangeModeGame()
+        {
+            InitializeComponent();
+            radioButtons = new List<RadioButton>
+            {
+                radioButton1, radioButton2, radioButton3
+            };
+        }
+
+        private void ChangeModeGame_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
